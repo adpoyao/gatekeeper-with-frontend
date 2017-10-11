24852561847f93003047782ea3a5dc46f6d7b1b5
Elias Mason <eliasjmason@gmail.com>
2017-10-10 17:54:42 -0700

    Add comments to frontend js

--- public/index.js
+++ public/index.js
@@ -1,6 +1,10 @@
 /* global Cookies */
 'use strict';
 
+// This will store a boolean that reports 
+// if the 'isLoggedIn' cookie exists.
+// it will be updated on page load,
+// and upon form submission
 let isLoggedIn;
 
 function generateNavHTML() {
@@ -28,10 +32,16 @@ function generateFeedback() {
   return feedback;
 }
 
-function renderForm({showFeedback}) {
+// Our parent render function takes an argument 
+// representing a collection of render options.
+// showFeedback is a true/false boolean from that object.
+
+function renderForm(showFeedback) {
   $('form')
     .find('input')
     .val('')
+    // end allows us to go back to our first query,
+    // the form.
     .end()
     .prop('hidden', isLoggedIn);
 
@@ -46,6 +56,10 @@ function handleLogin() {
   $('form').on('submit', function(e) {
     e.preventDefault();
 
+    // Here, we quickly convert the form 
+    // into a query string. 
+    // See $ docs on .serialize():
+    // https://api.jquery.com/serialize/
     const serializedForm = $('form').serialize();
     const authHeader = { 
       'x-username-and-password': serializedForm 
@@ -56,7 +70,11 @@ function handleLogin() {
       method: 'POST',
       headers: authHeader,
       success: function(res) {
+        // update our global isLoggedIn
         isLoggedIn = Cookies.get('isLoggedIn') === 'true';
+        // since we've attempted to use the form,
+        // we want to show the feedback section of
+        // our app.
         renderApp({showFeedback: true});
         console.log(res);
       }
@@ -66,12 +84,20 @@ function handleLogin() {
   });
 }
 
-function renderApp(opts = {showFeedback: false}) {
+// this function is simple now,
+// but could be useful for organizing render
+// if the UI gets more complex
+function renderApp(opts) {
   renderNav();
-  renderForm(opts);
+  renderForm(opts.showFeedback);
 }
 $(function() {
+  // check the isLoggedIn cookie,
+  // in case there's a user from a previous visit
   isLoggedIn = Cookies.get('isLoggedIn') === 'true';
-  renderApp();
+
+  // on first render, there is no need
+  // to show the feedback
+  renderApp({showFeedback: false});
   handleLogin();
 });
